28f9f851d03802f6cca6b26103310669c771036a
JuanPa <jprojaschinchilla@gmail.com>
2021-10-14 22:44:25 -0600

    ya esta hecha la conexion de node con la base de datos

--- database/script.sql
+++ database/script.sql
@@ -1,9 +1,26 @@
-create table prueba(
-    EVALUATION_ID    NUMBER(8,0), 
-    EMPLOYEE_ID      NUMBER(6,0), 
-    EVALUATION_DATE  DATE, 
-    JOB_ID           VARCHAR2(10), 
-    MANAGER_ID       NUMBER(6,0), 
-    DEPARTMENT_ID    NUMBER(4,0),
-    TOTAL_SCORE      NUMBER(3,0)
+create table TipoUsuario(
+    id_tipousuario int not null primary key,
+    tipo varchar(15)
 );
+create sequence id_tipousuario start with 1;
+
+create table Usuario(
+    id_usuario int not null primary key,
+    username varchar(15),
+    password varchar(15),
+    id_tipousuario int not null,
+    constraint fk_tipo 
+        foreign key (id_tipousuario) 
+        references TipoUsuario(id_tipousuario)
+);
+create sequence id_usuario start with 1;
+
+
+insert into tipousuario values (id_tipousuario.nextval, 'administrador');
+insert into tipousuario values (id_tipousuario.nextval, 'aplicante');
+insert into tipousuario values (id_tipousuario.nextval, 'coordinador');
+insert into tipousuario values (id_tipousuario.nextval, 'guest');
+insert into tipousuario values (id_tipousuario.nextval, 'revisor');
+
+insert into usuario values (id_usuario.nextval, 'admin', 'admin', 1);
+
--- test/test.js
+++ test/test.js
@@ -1,43 +1,30 @@
-configuracion = {
+var oracledb = require("oracledb");
+
+var connAttrs = {
     user: "juanpa",
     password: "juanpa",
-    connectString: "juanpa/juanpa@//localhost:1521/ORCLCDB.localdomain",
+    connectString: "localhost:1521/ORCL18",
 };
 
-const oracledb = require('oracledb');
-
-// On Windows and macOS, you can specify the directory containing the Oracle
-// Client Libraries at runtime, or before Node.js starts.  On other platforms
-// the system library search path must always be set before Node.js is started.
-// See the node-oracledb installation documentation.
-// If the search path is not correct, you will get a DPI-1047 error.
-if (process.platform === 'win32') { // Windows
-    oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_19_11' });
-} else if (process.platform === 'darwin') { // macOS
-    oracledb.initOracleClient({ libDir: process.env.HOME + '/Downloads/instantclient_19_8' });
-}
-
-async function run() {
-
-    let connection;
+async function connect(consulta) {
+    console.log(consulta);
+    let conn;
 
     try {
-        // Get a non-pooled connection
-        connection = await oracledb.getConnection(configuracion);
-
-        console.log('Connection was successful!');
-
-    } catch (err) {
-        console.error(err);
-    } finally {
-        if (connection) {
-            try {
-                await connection.close();
-            } catch (err) {
-                console.error(err);
+        conn = await oracledb.getConnection(connAttrs);
+        let result = await conn.execute(
+            consulta,
+            [], // no binds
+            {
+                outFormat: oracledb.OBJECT,
             }
-        }
+        );
+
+        console.log(result);
+        return { "status": 200, "data": result.rows };
+    } catch (error) {
+        console.log(error);
+        return { "status": 400, "message": error.message };
     }
 }
-
-run();
+connect("select * from usuario");
